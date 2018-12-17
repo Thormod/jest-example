72674f4fc30f206df540f96345bb650ee742ee3d
Juan Sebastian Zapata Tamayo <thjodhild@gmail.com>
2018-12-17 14:20:16 -0500

    Adding tslint

--- test/base.spec.ts
+++ test/base.spec.ts
@@ -1,87 +1,83 @@
-// const agent = require('superagent-promise')(require('superagent'), Promise);
-import agent = require('superagent');
-import statusCode = require('http-status-codes');
-
+import superagent = require('superagent');
+import httpStatusCodes = require('http-status-codes');
 
 describe('Given a website for API testing', () => {
-    test('When consume POST service with query parameters', () => {
-        const query = {
-            name: 'John',
-            age: '31',
-            city: 'New York'
-        };
+  test('When consume POST service with query parameters', () => {
+    const query = {
+      name: 'John',
+      age: '31',
+      city: 'New York'
+    };
 
-        return agent
+    return superagent
         .post('https://httpbin.org/post')
         .send(query)
         .then((response) => {
-            expect(response.status).toBe(statusCode.OK);
-            expect(response.body.json).toMatchSnapshot();
+          expect(response.status).toBe(httpStatusCodes.OK);
+          expect(response.body.json).toMatchSnapshot();
         });
-    });
+  });
 
-    test('When consume GET service with query parameters', () => {
-        const query = {
-            name: 'John',
-            age: '31',
-            city: 'New York'
-        };
+  test('When consume GET service with query parameters', () => {
+    const query = {
+      name: 'John',
+      age: '31',
+      city: 'New York'
+    };
 
-        return agent.get('https://httpbin.org/get')
+    return superagent.get('https://httpbin.org/get')
         .query(query)
         .then((response) => {
-            expect(response.status).toBe(statusCode.OK);
-            expect(response.body.args).toMatchSnapshot();
+          expect(response.status).toBe(httpStatusCodes.OK);
+          expect(response.body.args).toMatchSnapshot();
         });
-    });
+  });
 
-    test('When consume DELETE service with query parameters', () => {
-        const body = {
-          name: 'Sebastián',
-          age: 23,
-          city: 'Medellin'
-        };
-    
-        return agent
+  test('When consume DELETE service with query parameters', () => {
+    const body = {
+      name: 'Sebastián',
+      age: 23,
+      city: 'Medellin'
+    };
+
+    return superagent
         .del('https://httpbin.org/delete')
         .send(body)
         .then((response) => {
-            expect(response.status).toBe(statusCode.OK);
-            expect(response.body.json).toMatchSnapshot();
+          expect(response.status).toBe(httpStatusCodes.OK);
+          expect(response.body.json).toMatchSnapshot();
         });
-    });
+  });
 
-    test('Consume PUT Service with query parameters', () => {
-        const body = {
-            name: 'Sebastián',
-            age: 23,
-            city: 'Medellin'
-        };
+  test('Consume PUT Service with query parameters', () => {
+    const body = {
+      name: 'Sebastián',
+      age: 23,
+      city: 'Medellin'
+    };
 
-        return agent
+    return superagent
         .put('https://httpbin.org/put')
         .send(body)
         .then((response) => {
-            expect(response.status).toBe(statusCode.OK);
-            expect(response.body.json).toMatchSnapshot();
+          expect(response.status).toBe(httpStatusCodes.OK);
+          expect(response.body.json).toMatchSnapshot();
         });
-    });
-    
-    test('Consume PATCH Service with query parameters', () => {
-        const body = {
-          name: 'Sebastián',
-          age: 23,
-          city: 'Medellin'
-        };
-    
-        return agent
+  });
+
+  test('Consume PATCH Service with query parameters', () => {
+    const body = {
+      name: 'Sebastián',
+      age: 23,
+      city: 'Medellin'
+    };
+
+    return superagent
         .patch('https://httpbin.org/patch')
         .send(body)
         .then((response) => {
-            expect(response.status).toBe(statusCode.OK);
-            expect(response.body.json).toMatchSnapshot();
+          expect(response.status).toBe(httpStatusCodes.OK);
+          expect(response.body.json).toMatchSnapshot();
         });
-    });
+  });
 });
-
-
